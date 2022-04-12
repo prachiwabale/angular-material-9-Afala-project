2d92b076313741f3ffce02c236d9bf7e254244f2
prachiwabale <prachiwabale2091993@gmail.com>
2022-04-12 11:21:46 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-material-9-zdouif
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-material-9-zdouif)
---
+++ src/app/aflac/aflac.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aflac',
+  templateUrl: './aflac.component.html',
+  styleUrls: ['./aflac.component.css']
+})
+export class AflacComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
